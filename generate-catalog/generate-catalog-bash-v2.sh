19869be7fbefb458a967de6e7fcce641773ddcdb
Anton Tcvetkov <Anton.Tcvetkov@adidas.com>
2024-03-05 19:07:29 +0100

    added some sole applications #5

--- generate-catalog/generate-catalog-bash-v2.sh
+++ generate-catalog/generate-catalog-bash-v2.sh
@@ -4,51 +4,35 @@
 NAMESPACE="glass"
 
 # Define the name for the summary file
-SUMMARY_FILE="../all-components.yaml"
+SUMMARY_FILE="../all-components-v2.yaml"
 
 # Remove the summary file if it already exists
 [ -f "$SUMMARY_FILE" ] && rm "$SUMMARY_FILE"
 
-# Create a list to hold the targets
-targets=()
-
 # Iterate through each deployment in the namespace
-for deployment in $(kubectl get deployments -n $NAMESPACE -o=jsonpath='{.items[*].metadata.name}'); do
+# for deployment in $(kubectl get deployments -n $NAMESPACE -o=jsonpath='{.items[*].metadata.name}'); do
+for deployment in $(kubectl get deployments -n $NAMESPACE -o=jsonpath='{range .items[?(@.metadata.labels.framework=="sole")]}{.metadata.name}{"\n"}{end}'); do
     label_app_kubernetes=$(kubectl get deployment $deployment -n $NAMESPACE -o=jsonpath='{.metadata.labels.app\.kubernetes\.io\/name}')
 
-    # Create YAML content
-    cat > "../catalog-entries/$deployment.yaml" <<EOF
+    # Update YAML content
+    cat >> "$SUMMARY_FILE" <<EOF
 ---
 apiVersion: backstage.io/v1alpha1
 kind: Component
 metadata:
-  name: $label_app_kubernetes
+  name: $deployment
+  namespace: $NAMESPACE
   annotations:
-    backstage.io/kubernetes-label-selector: app.kubernetes.io/name=$label_app_kubernetes,framework=sole
+    # backstage.io/kubernetes-label-selector: app.kubernetes.io/name=$label_app_kubernetes,framework=sole
+    backstage.io/kubernetes-label-selector: app.kubernetes.io/instance=$deployment,framework=sole
+  tags:
+    - sole
+    - $label_app_kubernetes
 spec:
   type: service
   lifecycle: experimental
   owner: exp-core
 EOF
-
-    # Add the deployment.yaml file to the list of targets
-    targets+=("./catalog-entries/$deployment.yaml")
-done
-
-# Create YAML content for the summary file
-cat > "$SUMMARY_FILE" <<EOF
-apiVersion: backstage.io/v1alpha1
-kind: Location
-metadata:
-  name: all-sole-components
-  description: A collection of all Sole Backstage example components
-spec:
-  targets:
-EOF
-
-# Add the list of targets to the summary file
-for target in "${targets[@]}"; do
-    echo "    - $target" >> "$SUMMARY_FILE"
 done
 
 echo "Summary file '$SUMMARY_FILE' created successfully."
